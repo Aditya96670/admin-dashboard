7c3414cd4d7ccd6dedd12e36fa3984a90fa1c53c
Shubham Bhatt <shubhambhatt92021@gmail.com>
2025-11-24 14:49:10 +0530

    private router add

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 import AdminLogin from "./pages/AdminLogin";
 import AdminDashboard from "./pages/AdminDashboard";
+import PrivateRouter from "./components/PrivateRouter";
 
 const App = () => {
   return (
@@ -13,7 +14,14 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Navigate to="/dashboard" replace />} />
         <Route path="/login" element={<AdminLogin />} />
-        <Route path="/dashboard" element={<AdminDashboard />} />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRouter>
+              <AdminDashboard />
+            </PrivateRouter>
+          }
+        />
       </Routes>
     </Router>
   );
